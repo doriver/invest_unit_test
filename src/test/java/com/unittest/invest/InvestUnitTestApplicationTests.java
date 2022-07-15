72b8be21de6fce099285f5f50422ef6a7763d8a7
doriver <tkdduq241@naver.com>
2022-07-15 19:39:40 +0900

    was생성

---
+++ src/test/java/com/unittest/invest/InvestUnitTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unittest.invest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestUnitTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
